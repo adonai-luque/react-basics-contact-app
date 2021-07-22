cd22817f27a5569619b2947b7885e0873ccadb3c
adonai-luque <akluqueprieto@gmail.com>
2021-07-22 01:18:47 -0500

    search functionality added to ContactList component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 
 export default function ContactList({ contacts, removeContactHandler }) {
+  const [keyword, setKeyword] = useState("")
+  function keywordFound(contact, keyword) {
+    const name = contact.name.toLowerCase()
+    const email = contact.email.toLowerCase()
+    const term = keyword.toLowerCase()
+    return ((name.search(term) !== -1) || (email.search(term)) !== -1)
+  }
   return (
     <div className="main">
       <div className="header">
@@ -10,9 +18,21 @@ export default function ContactList({ contacts, removeContactHandler }) {
           <button className="ui button blue right">Add Contact</button>
         </Link>
       </div>
+      <div className="ui search">
+        <div className="ui icon input">
+          <input
+            className="prompt"
+            type="text"
+            placeholder="Search Contacts"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+          />
+          <i className="search icon"></i>
+        </div>
+      </div>
       <div className="ui celled list">
         {contacts.map((contact) => {
-          return (
+          return keywordFound(contact, keyword) ? (
             <ContactCard
               contact={contact}
               removeContactHandler={function () {
@@ -20,7 +40,7 @@ export default function ContactList({ contacts, removeContactHandler }) {
               }}
               key={contact.id}
             />
-          );
+          ) : null
         })}
       </div>
     </div>
